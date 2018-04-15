309a8bc8ecf4d9d0a1be695f4160a6cd3fe0f634
Ben Williams <deserblade@gmail.com>
2018-04-15 08:17:34 -0700

    Navbar Complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 
+import { AppComponent } from "./app.component";
 
-import { AppComponent } from './app.component';
-
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { NavbarComponent } from "./navbar/navbar.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MDBBootstrapModule.forRoot()
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [ NO_ERRORS_SCHEMA ]
 })
 export class AppModule { }
